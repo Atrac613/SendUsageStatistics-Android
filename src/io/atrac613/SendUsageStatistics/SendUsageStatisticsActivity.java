83d463bd59604f538049e226bfed50fafc5b037b
atrac613 <atrac630>
2012-06-07 00:51:46 +0900

    fix indenting.

--- src/io/atrac613/SendUsageStatistics/SendUsageStatisticsActivity.java
+++ src/io/atrac613/SendUsageStatistics/SendUsageStatisticsActivity.java
@@ -16,13 +16,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class SendUsageStatisticsActivity extends Activity {
-	/** Google Analytics */
+    /** Google Analytics */
     GoogleAnalyticsTracker tracker;
-	
-	public static final String SEND_USAGE_STATISTICS_ALERT_KEY = "send_usage_statistics_alert_key";
-	public static final String SEND_USAGE_STATISTICS_KEY = "send_usage_statistics_key";
-	public static final String SEND_USAGE_STATISTICS_PREF = "send_usage_statistics_pref";
-	
+    
+    public static final String SEND_USAGE_STATISTICS_ALERT_KEY = "send_usage_statistics_alert_key";
+    public static final String SEND_USAGE_STATISTICS_KEY = "send_usage_statistics_key";
+    public static final String SEND_USAGE_STATISTICS_PREF = "send_usage_statistics_pref";
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,20 +32,20 @@ public class SendUsageStatisticsActivity extends Activity {
         tracker = GoogleAnalyticsTracker.getInstance();
         
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-    	boolean send_usage_statistics = prefs.getBoolean(SEND_USAGE_STATISTICS_KEY, false);
+        boolean send_usage_statistics = prefs.getBoolean(SEND_USAGE_STATISTICS_KEY, false);
         
-    	if (send_usage_statistics) {
-    		// Start the tracker in manual dispatch mode...
+        if (send_usage_statistics) {
+            // Start the tracker in manual dispatch mode...
             tracker.startNewSession(AppConstants.GOOGLE_ANALYTICS_TRACKING_ID, AppConstants.GOOGLE_ANALYTICS_DISPATCH_INTERVAL, this);
             
             tracker.trackPageView("/SendUsageStatisticsActivity");
-    	}
+        }
         
         Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            	showSendUsageStatisticsAlert(true);
+                showSendUsageStatisticsAlert(true);
             }
         });
         
@@ -54,10 +54,10 @@ public class SendUsageStatisticsActivity extends Activity {
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-    	Intent intent = new Intent(SendUsageStatisticsActivity.this, SettingsActivity.class);
-    	intent.setAction(Intent.ACTION_VIEW);
-    	startActivity(intent);
-    	
+        Intent intent = new Intent(SendUsageStatisticsActivity.this, SettingsActivity.class);
+        intent.setAction(Intent.ACTION_VIEW);
+        startActivity(intent);
+        
         return true;
     }
     
@@ -69,19 +69,19 @@ public class SendUsageStatisticsActivity extends Activity {
     }
     
     private void showSendUsageStatisticsAlert(boolean force){
-    	final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-    	boolean send_usage_statistics_alert = prefs.getBoolean(SEND_USAGE_STATISTICS_ALERT_KEY, false);
-    	
-    	if (!send_usage_statistics_alert || force) {
-    		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean send_usage_statistics_alert = prefs.getBoolean(SEND_USAGE_STATISTICS_ALERT_KEY, false);
+        
+        if (!send_usage_statistics_alert || force) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setIcon(android.R.drawable.ic_dialog_info);
             builder.setMessage(R.string.message_send_usage_statistics);
             
             builder.setPositiveButton(R.string.button_yes, new DialogInterface.OnClickListener(){
                 @Override
                 public void onClick(DialogInterface dialog, int which){
-                	SharedPreferences.Editor editor = prefs.edit();
-                	editor.putBoolean(SEND_USAGE_STATISTICS_ALERT_KEY, true);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putBoolean(SEND_USAGE_STATISTICS_ALERT_KEY, true);
                     editor.putBoolean(SEND_USAGE_STATISTICS_KEY, true);
                     editor.commit();
                 }
@@ -89,13 +89,13 @@ public class SendUsageStatisticsActivity extends Activity {
             builder.setNegativeButton(R.string.button_no, new DialogInterface.OnClickListener(){
                 @Override
                 public void onClick(DialogInterface dialog, int which){
-                	SharedPreferences.Editor editor = prefs.edit();
-                	editor.putBoolean(SEND_USAGE_STATISTICS_ALERT_KEY, true);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putBoolean(SEND_USAGE_STATISTICS_ALERT_KEY, true);
                     editor.putBoolean(SEND_USAGE_STATISTICS_KEY, false);
                     editor.commit();
                 }
             });
             builder.create().show();
-    	}
+        }
     }
 }
